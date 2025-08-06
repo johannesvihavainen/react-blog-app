1c942f46cf7ae3056c8a6257102ad506ba7db251
Johannes Vihavainen <johannesvihavainen@outlook.com>
2025-08-06 17:03:58 +0300

    functional blog website built with MERN

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,17 +1,25 @@
-export default function Post() {
-    return (
-        <div className="post">
-        <div className="image">
-          <img src="https://images.unsplash.com/photo-1504233529578-6d46baba6d34?q=80&w=2674&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        </div>
-        <div className="texts">
-          <h2>Near Reine, Norway the Eliassen Rorbuer overlooks Moskenesøya.</h2>
-          <p className="info">
-            <a className='author'>Eliassen Rorbuer</a>
-            <time >08-04-2025 17:15</time>
-          </p>
-          <p className='summary'>This is just one of many tiny fishing villages between the Lofoten Islands which is a fantastic place to eat or sleep while visiting the Arctic Circle.</p>
-        </div>
+import { formatISO9075 } from 'date-fns'
+import {Link} from 'react-router-dom'
+
+export default function Post({ _id, title, summary, cover, content, createdAt, author }) {
+  return (
+    <div className="post">
+      <div className="image">
+        <Link to={`/post/${_id}`}>
+          <img src={'http://localhost:4000/' + cover} alt="" />
+        </Link>
+
       </div>
-    )
+      <div className="texts">
+        <Link to={`/post/${_id}`}>
+          <h2>{title}</h2>
+        </Link>
+        <p className="info">
+          <a className='author'>{author?.username || 'Unknown Author'}</a>
+          <time>{formatISO9075(new Date(createdAt))}</time>
+        </p>
+        <p className='summary'>{summary}</p>
+      </div>
+    </div>
+  )
 }
